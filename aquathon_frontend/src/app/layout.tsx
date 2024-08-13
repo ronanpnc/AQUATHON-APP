905331b4e6ee103bdc767c51a9a6d1300c323424
Tharo Chan <tharochanrith@gmail.com>
2024-08-13 13:49:59 +0700

    refactor: improve and clean up

--- aquathon_frontend/src/app/layout.tsx
+++ aquathon_frontend/src/app/layout.tsx
@@ -7,7 +7,7 @@ import { TopNav } from '~/components/layouts/TopNav';
 export const metadata: Metadata = {
   title: 'Aquathon App',
   description: 'Race Time Tracker',
-  icons: [{ rel: 'icon', url: '/favicon.ico' }],
+  icons: [{ rel: 'icon', url: '/assets/icons/ic_logo.svg' }],
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
--- aquathon_frontend/src/components/layouts/TopNav.tsx
+++ aquathon_frontend/src/components/layouts/TopNav.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   NavigationMenu,
   NavigationMenuItem,
@@ -6,50 +8,49 @@ import {
 } from '@radix-ui/react-navigation-menu';
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import * as React from 'react';
 
 import { navigationMenuTriggerStyle } from '../ui/navigation-menu';
 
 export function TopNav() {
+  const pathname = usePathname();
+
+  const menuItems = [
+    { href: '/racer/setup', label: 'Setup' },
+    { href: '/racer/competitors', label: 'Competitors' },
+    { href: '/racer/timers', label: 'Timers' },
+    { href: '/racer/dashboard', label: 'Dashboard' },
+    { href: '/racer/result', label: 'Result' },
+  ];
+
   return (
     <NavigationMenu className='w-full bg-white shadow-md'>
       <NavigationMenuList className='flex flex-row justify-start items-center space-x-4 py-4 px-6 h-16'>
-        <NavigationMenuItem>
+        <NavigationMenuItem className='mr-48'>
           <Link href='/' passHref>
             <Image
               src='/assets/icons/ic_logo.svg'
               alt='Logo'
               width={210}
-              height={120}
+              height={140}
               className='cursor-pointer pl-4'
             />
           </Link>
         </NavigationMenuItem>
-        <NavigationMenuItem className='pl-48'>
-          <Link href='/racer/setup' legacyBehavior passHref>
-            <NavigationMenuLink className={`${navigationMenuTriggerStyle()} text-base`}>Setup</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href='/racer/competitors' legacyBehavior passHref>
-            <NavigationMenuLink className={`${navigationMenuTriggerStyle()} text-base`}>Competitors</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href='/racer/timers' legacyBehavior passHref>
-            <NavigationMenuLink className={`${navigationMenuTriggerStyle()} text-base`}>Timers</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href='/racer/dashboard' legacyBehavior passHref>
-            <NavigationMenuLink className={`${navigationMenuTriggerStyle()} text-base`}>Dashboard</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href='/racer/result' legacyBehavior passHref>
-            <NavigationMenuLink className={`${navigationMenuTriggerStyle()} text-base`}>Result</NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
+        {menuItems.map((item) => (
+          <NavigationMenuItem key={item.href} className='px-4'>
+            <Link href={item.href} legacyBehavior passHref>
+              <NavigationMenuLink
+                className={`${navigationMenuTriggerStyle()} text-base ${
+                  pathname === item.href ? 'border-b-2 border-current' : ''
+                }`}
+              >
+                {item.label}
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        ))}
       </NavigationMenuList>
     </NavigationMenu>
   );
