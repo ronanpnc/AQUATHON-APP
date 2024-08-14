4f3be0bb480f0e7630cec666fb91b037a457f899
TSPanha <tepspanha@gmail.com>
2024-08-14 10:29:06 +0700

    AA-10 feat: sidebar feature

---
+++ aquathon_frontend/src/components/layouts/sidebar.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { useStore } from 'zustand';
+import { useSidebarToggle } from '~/hooks/use-sidebar-toggle';
+
+import { cn } from '~/lib/utils';
+import { Button } from '../ui/button';
+import { SidebarToggle } from './sidebar-toggle';
+import { Menu } from './menu';
+
+export function Sidebar() {
+  const sidebar = useStore(useSidebarToggle, (state) => state);
+
+  if (!sidebar) return null;
+
+  return (
+    <aside
+      className={cn(
+        'fixed top-0 left-0 z-20 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300 bg-white',
+        sidebar?.isOpen === false ? 'w-[90px]' : 'w-64',
+      )}
+    >
+      <SidebarToggle isOpen={sidebar?.isOpen} setIsOpen={sidebar?.setIsOpen} />
+      <div className='relative h-full flex flex-col px-3 py-4 shadow-md dark:shadow-zinc-800'>
+        <Button
+          className={cn(
+            'transition-transform ease-in-out duration-300 mb-1',
+            sidebar?.isOpen === false ? 'translate-x-1' : 'translate-x-0',
+          )}
+          variant='link'
+          asChild
+        >
+          <Link href='/' className='flex items-center gap-2'>
+            <Image
+              src='/assets/icons/ic_logo_sidebar.svg'
+              alt='Logo'
+              width={210}
+              height={120}
+              className={cn('cursor-pointer', sidebar?.isOpen === true
+                ? "-translate-x-96 opacity-0 hidden"
+                : "translate-x-0 opacity-100")}
+            />
+            <Image
+              src='/assets/icons/ic_logo.svg'
+              alt='Logo'
+              width={210}
+              height={120}
+              className={cn('cursor-pointer', sidebar?.isOpen === false
+                ? "-translate-x-96 opacity-0 hidden"
+                : "translate-x-0 opacity-100")}
+            />
+          </Link>
+        </Button>
+        <Menu
+          isOpen={sidebar?.isOpen}
+        />
+      </div>
+    </aside>
+  );
+}
