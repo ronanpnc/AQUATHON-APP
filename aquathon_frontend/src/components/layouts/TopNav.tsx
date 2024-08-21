79a9334afa7762b491701d8d2eff8a73a8750825
Tharo Chan <tharochanrith@gmail.com>
2024-08-21 13:50:26 +0700

    style: adjust header size (AA-50)

--- aquathon_frontend/src/components/layouts/TopNav.tsx
+++ aquathon_frontend/src/components/layouts/TopNav.tsx
@@ -7,13 +7,13 @@ import React from 'react';
 export function TopNav() {
   return (
     <nav className='w-full bg-white shadow-md fixed top-0 left-0 right-0 z-10'>
-      <div className='flex items-center h-20 px-6'>
+      <div className='flex items-center h-16 px-6'>
         <Link href='/' passHref>
           <Image
             src='/assets/icons/ic_logo.svg'
             alt='Logo'
-            width={180}
-            height={50}
+            width={160}
+            height={45}
             className='cursor-pointer'
           />
         </Link>
