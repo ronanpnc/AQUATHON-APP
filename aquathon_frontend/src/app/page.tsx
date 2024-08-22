34f520574d3b570b782894981d55fab74d7b3f1d
Tharo Chan <tharochanrith@gmail.com>
2024-08-22 10:14:58 +0700

    refactor: use route groups (AA-49)

--- aquathon_frontend/src/app/page.tsx
+++ aquathon_frontend/src/app/page.tsx
@@ -1,9 +1,18 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 export default function HomePage() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('/races');
+  };
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white'>
-      <div className='text-4xl font-bold'>In Progress</div>
+      <button onClick={handleClick}>View Races</button>
     </main>
   );
 }
