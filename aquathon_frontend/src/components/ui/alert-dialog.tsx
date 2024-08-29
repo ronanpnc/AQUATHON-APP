d0929a9cddf2f2d4a9552b5b59a1c27c85c0ccec
Tharo Chan <tharochanrith@gmail.com>
2024-08-29 11:34:46 +0700

    refactor: improve import (AA-101)

--- aquathon_frontend/src/components/ui/alert-dialog.tsx
+++ aquathon_frontend/src/components/ui/alert-dialog.tsx
@@ -3,9 +3,9 @@
 import * as AlertDialogPrimitive from '@radix-ui/react-alert-dialog';
 import * as React from 'react';
 
-import { buttonVariants } from './button';
+import { cn } from '@/lib/utils';
 
-import { cn } from '~/lib/utils';
+import { buttonVariants } from './button';
 
 const AlertDialog = AlertDialogPrimitive.Root;
 
