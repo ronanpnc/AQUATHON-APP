f2d69d4026d28e58691900376e78c69cbad54c69
Tharo Chan <tharochanrith@gmail.com>
2024-08-29 14:58:48 +0700

    feat: added race form (AA-101)

--- aquathon_frontend/src/components/race/AddRaceButton.tsx
+++ aquathon_frontend/src/components/race/AddRaceButton.tsx
@@ -1,37 +1,21 @@
-import {
-  AlertDialog,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from '@radix-ui/react-alert-dialog';
 import { Plus } from 'lucide-react';
-import { useState } from 'react';
 
 export function AddRaceButton() {
-  const [open, setOpen] = useState(false);
-
   return (
     <div key='add-race-button'>
-      <AlertDialog open={open} onOpenChange={setOpen}>
-        <AlertDialogTrigger>
-          <ActionButton />
-        </AlertDialogTrigger>
-        <AlertDialogContent className='fixed inset-0 flex items-center justify-center' onClick={() => setOpen(false)}>
-          <div className='bg-white p-6 rounded shadow-lg' onClick={(e) => e.stopPropagation()}>
-            <AlertDialogTitle>Confirm Action</AlertDialogTitle>
-            <AlertDialogDescription>Are you sure you want to proceed with this action?</AlertDialogDescription>
-            <AlertDialogDescription>Are you sure you want to proceed with this action?</AlertDialogDescription>
-          </div>
-        </AlertDialogContent>
-      </AlertDialog>
+      <a
+        href='/races/create'
+        className='rounded-full h-12 w-12 border-2 border-gray-300 flex items-center justify-center shadow-md'
+      >
+        <Plus className='h-8 w-8' />
+      </a>
     </div>
   );
 }
 
 function ActionButton() {
   return (
-    <div className='rounded-full h-12 w-12 border-2 border-grey-500 flex items-center justify-center drop-shadow-md'>
+    <div className='rounded-full h-12 w-12 border-2 border-gray-300 flex items-center justify-center shadow-md'>
       <Plus className='h-8 w-8' />
     </div>
   );
