d51830fbae7c1ef988f9578e80fdce99ddd48210
Tharo Chan <tharochanrith@gmail.com>
2024-09-02 11:07:46 +0700

    feat: added create race nav view (AA-101)

--- aquathon_frontend/src/app/(race)/races/(crud)/create/page.tsx
+++ aquathon_frontend/src/app/(race)/races/(crud)/create/page.tsx
@@ -1,10 +1,18 @@
+import { X } from 'lucide-react';
+import Link from 'next/link';
+
 import CreateRaceForm from '@/components/race/CreateRaceForm';
 
 export default function CreateRacePage() {
   return (
     <div>
       <nav className='w-full bg-white shadow-md fixed top-0 left-0 right-0 z-10'>
-        <div className='flex items-center h-16 px-6 text-lg font-bold'>Create New Race</div>
+        <div className='flex items-center h-16 px-6 text-xl font-bold'>
+          <Link href='/races'>
+            <X strokeWidth={3} />
+          </Link>
+          <span className='ml-4'>Create New Race</span>
+        </div>
       </nav>
       <div className='mt-12'>
         <CreateRaceForm />
