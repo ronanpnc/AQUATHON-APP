381f342a31a89b39564a14875325fba319e9dc62
TSPanha <tepspanha@gmail.com>
2024-09-06 20:56:29 +0700

    feat: develop the empty state race views and add some new icons

--- aquathon_frontend/src/app/(root)/page.tsx
+++ aquathon_frontend/src/app/(root)/page.tsx
@@ -1,18 +1,22 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 import React from 'react';
 
+import { TopNav } from '@/components/layouts/TopNav';
+import { AddRaceButton } from '@/components/race/AddRaceButton';
+
 export default function HomePage() {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push('/races');
-  };
-
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white'>
-      <button onClick={handleClick}>View Races</button>
+    <main className='flex min-h-screen flex-col items-center justify-center'>
+      <TopNav />
+      <div className='items-center flex-col flex'>
+        <Image src='/assets/icons/ic_cross_race.svg' alt='Logo' width={250} height={250} className='cursor-pointer' />
+        <span className='text-xl font-medium italic'>“Start By Creating a Race”</span>
+      </div>
+      <div className='fixed bottom-4 right-4'>
+        <AddRaceButton />
+      </div>
     </main>
   );
 }
