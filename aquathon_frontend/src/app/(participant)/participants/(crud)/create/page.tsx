b56ff43ac656c4ae7edc4a6ee6942906c5033259
TSPanha <tepspanha@gmail.com>
2024-09-08 10:22:54 +0700

    refactor: change the folder (AA-110)

---
+++ aquathon_frontend/src/app/(participant)/participants/(crud)/create/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { X } from 'lucide-react';
+import Link from 'next/link';
+
+import CreateParticipantForm from '@/components/participant/CreateParticipantForm';
+
+export default function CreateParticipant() {
+  return (
+    <div className='bg-[#F3F6FB] h-screen'>
+      <nav className='flex items-center justify-between sticky top-0 bg-[#7E83DE] text-white border-b p-4 border-gray-300 shadow-md mb-4'>
+        <div className='flex items-center'>
+          <Link href='/races'>
+            <X />
+          </Link>
+          <h1 className='text-xl font-bold ml-4'>Create New Participant</h1>
+        </div>
+      </nav>
+      <div>
+        <CreateParticipantForm raceId={''} />
+      </div>
+    </div>
+  );
+}
