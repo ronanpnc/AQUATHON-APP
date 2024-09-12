37979e32552505b187a068c901417542a98ae193
TSPanha <tepspanha@gmail.com>
2024-09-12 21:46:03 +0700

    feat: add more info age and gender at the participant card (AA-232)

--- aquathon_frontend/src/components/participant/ParticipantCard.tsx
+++ aquathon_frontend/src/components/participant/ParticipantCard.tsx
@@ -2,38 +2,69 @@ import Link from 'next/link';
 
 import { Participant } from '@/domains/participant/interface';
 
-const ParticipantCard: React.FC<{ participant: Participant , raceId:string}> = ({ participant, raceId }) => {
-  const { _id, firstName, lastName, bib ,colour} = participant;
+const calculateAge = (dob: string) => {
+  const birthDate = new Date(dob);
+  const today = new Date();
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const monthDifference = today.getMonth() - birthDate.getMonth();
+  if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+};
+
+const ParticipantCard: React.FC<{ participant: Participant; raceId: string }> = ({ participant }) => {
+  const { _id, firstName, lastName, bib, colour, gender, dateOfBirth } = participant;
+  const age = calculateAge(dateOfBirth);
 
   return (
-    <Link href={`participants/${_id}/`} className='block px-5'>
-      <div className='flex items-center justify-between p-4 bg-white rounded-lg mb-4 hover:bg-gray-50 transition-colors duration-200'>
-        <div className='flex items-center'>
-          <ParticipantInfo firstname={firstName} lastname={lastName} number={bib}  colour={colour}/>
+    <Link href={`participants/${_id}/`} className='flex px-5'>
+      <div className='w-full h-20 px-5 py-4 bg-white rounded-2xl mb-5 hover:bg-gray-50 transition-colors duration-200'>
+        <div className='items-center'>
+          <ParticipantInfo
+            firstname={firstName}
+            lastname={lastName}
+            number={bib}
+            colour={colour}
+            gender={gender}
+            age={age}
+          />
         </div>
       </div>
     </Link>
   );
 };
 
-const ParticipantInfo: React.FC<{ firstname: string; lastname:string; number: number, colour:string }> = ({ firstname, lastname, number, colour }) => (
-  <div className='flex gap-2 items-center'>
-    <h1 className='text-lg font-semibold pr-2' style={{color: colour}}>{String(number).padStart(3, '0')}</h1>
-    <h3 className='text-lg font-semibold'>{firstname}</h3>
-    <h3 className='text-lg font-semibold'>{lastname}</h3>
-    {/*
-    <div className='flex items-center text-sm text-gray-500 space-x-4'>
-      <InfoItem icon={CalendarClock} number={number} />
-    </div>
-   */}
-  </div>
-);
+const ParticipantInfo: React.FC<{
+  firstname: string;
+  lastname: string;
+  number: number;
+  gender: string;
+  colour: string;
+  age: number;
+}> = ({ firstname, lastname, number, colour, gender, age }) => {
+  const genderDisplay = gender === 'male' ? 'M' : 'F';
+  const genderColor = gender === 'male' ? 'bg-blue-400' : 'bg-pink-400';
 
-const InfoItem: React.FC<{ icon: React.ElementType; number: number }> = ({ icon: Icon, number }) => (
-  <div className='flex items-center'>
-    <Icon size={16} className='mr-1' />
-    {number}
-  </div>
-);
+  return (
+    <div className='flex gap-8 items-center'>
+      <h1 className='text-lg font-semibold' style={{ color: colour }}>
+        {String(number).padStart(3, '0')}
+      </h1>
+      <div className='grid grid-rows-2 flex-grow space-y-1'>
+        <div className='inline-flex space-x-2 text-xl font-semibold'>
+          <span>{firstname}</span>
+          <span>{lastname}</span>
+        </div>
+        <div className='flex space-x-4'>
+          <h1 className='text-sm'>Age: {age}</h1>
+          <div className={`rounded-full w-5 h-5 justify-center items-center flex text-white ${genderColor}`}>
+            <h1 className='text-xs font-semibold'>{genderDisplay}</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ParticipantCard;
