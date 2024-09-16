203ef69023ef83d430ad3cf1261f07ffd4679499
Tharo Chan <tharochanrith@gmail.com>
2024-09-16 13:47:43 +0700

    feat: 1 step button (AA-175)

--- aquathon_frontend/src/app/(race)/races/[slug]/(racedetails)/(root)/time-tracking/(segments)/swimming/page.tsx
+++ aquathon_frontend/src/app/(race)/races/[slug]/(racedetails)/(root)/time-tracking/(segments)/swimming/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import Container from '@/components/Container';
+import TimeButton from '@/components/TimeTracking/TimeButton';
+
+const dummyParticipants = Array.from({ length: 50 }, (_, index) => ({
+  id: index + 1,
+  bibNumber: (101 + index).toString().padStart(3, '0'),
+}));
 
 export default function SwimmingTrackingPage() {
   return (
-    <div>Swimm</div>
-  )
+    <Container>
+      <h1 className='text-2xl font-bold mb-4'>Swimming Time Tracking</h1>
+      <div className='grid grid-cols-3 gap-4'>
+        {dummyParticipants.map((participant) => (
+          <TimeButton key={participant.id} bibNumber={participant.bibNumber} />
+        ))}
+      </div>
+    </Container>
+  );
 }
