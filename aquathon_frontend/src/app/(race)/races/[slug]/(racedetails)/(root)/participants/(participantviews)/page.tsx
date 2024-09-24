968e12ef1ac083a5ad1864ceeb2e373378f588e2
Tharo Chan <tharochanrith@gmail.com>
2024-09-24 20:56:44 +0700

    fix: update participant empty state

--- aquathon_frontend/src/app/(race)/races/[slug]/(racedetails)/(root)/participants/(participantviews)/page.tsx
+++ aquathon_frontend/src/app/(race)/races/[slug]/(racedetails)/(root)/participants/(participantviews)/page.tsx
@@ -9,7 +9,7 @@ import ParticipantCard from '@/components/participant/ParticipantCard';
 import { useParticipantList } from '@/services/participant.services';
 
 export default function MyParticipantPage() {
-  const param = useParams()
+  const param = useParams();
   const { isLoading, isError, data, error } = useParticipantList(param.slug as string);
 
   if (isLoading) {
@@ -21,15 +21,13 @@ export default function MyParticipantPage() {
   }
 
   return (
-    <div className=''>
-      <div className='pt-6'>{data?.map((participant) => <ParticipantCard key={participant._id} participant={participant} raceId={param.slug as string}  />)}</div>
-      <div>
-        {data?.length == 0 ?
-            <EmptyDataDisplay/>:
-
-            <div></div>
-        }
+    <div className='flex flex-col'>
+      <div className='pt-6'>
+        {data?.map((participant) => (
+          <ParticipantCard key={participant._id} participant={participant} raceId={param.slug as string} />
+        ))}
       </div>
+      <div className='flex-grow'>{data?.length === 0 ? <EmptyDataDisplay /> : <div></div>}</div>
       <div className='fixed bottom-4 right-4'>
         <AddParticipantButton />
       </div>
@@ -37,21 +35,21 @@ export default function MyParticipantPage() {
   );
 }
 
-
 const EmptyDataDisplay = () => {
-    return (
-        <div className='flex items-center justify-center fixed inset-0 z-10'>
-          <div className='flex flex-col items-center'>
-            <Image
-              src='/assets/icons/ic_cross_race.svg'
-              alt='Logo'
-              width={250}
-              height={250}
-              className='cursor-pointer'
-            />
-            <span className='text-xl font-medium italic mt-4'>No participant! Create First</span>
-          </div>
-        </div>
-
-    );
-}
+  return (
+    <div className='flex items-center justify-center fixed inset-0 z-5'>
+      <div className='flex flex-col items-center'>
+        <Image
+          src='/assets/icons/ic_empty_list Background Removed.png'
+          alt='Logo'
+          width={250}
+          height={250}
+          className='cursor-pointer'
+        />
+        <span className='text-xl font-medium italic mt-4 text-center'>
+          You have no participants at <br /> the moment
+        </span>
+      </div>
+    </div>
+  );
+};
